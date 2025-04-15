7a2bf9d3b0fced35af182775cf8c582532985eb2
MaryMachuma <machhmary173@gmail.com>
2025-04-15 12:16:07 +0300

    Add create and retrieve routes for plants

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,11 +17,30 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get("name"),
+            image=data.get("image"),
+            price=data.get("price")
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant:
+            return plant.to_dict(), 200
+        return {"error": "Plant not found"}, 404
+
+api.add_resource(Plants, "/plants")
+api.add_resource(PlantByID, "/plants/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
